93db90ca329e2daf46a7c91fac1694211a04033d
“ThomasSchnapka” <thomas.schnapka(at)outlook.com>
2021-06-09 08:06:28 +0200

    parameter tweakign

--- src/plotter.py
+++ src/plotter.py
@@ -9,67 +9,55 @@ import matplotlib.pyplot as plt
 import trajectories as traj
 import zero_moment_point as zmp
 
-#N = 200
 N = 20
-t = np.linspace(0, 2, N)
+T_hor = 3
+t = np.linspace(0, T_hor, N)
 leg_pos = np.array([[0.2, 0.2, -0.2, -0.2],
                     [0.1, -0.1, 0.1, -0.1],
                     [0, 0, 0, 0]])
 
+# get leg trajectories
 x = traj.get_x_trot(t) + np.tile(leg_pos[0], (N, 1)).T
 y = traj.get_y_trot(t) + np.tile(leg_pos[1], (N, 1)).T
 z = traj.get_z_trot(t) + np.tile(leg_pos[2], (N, 1)).T
 leg_state = traj.get_legstate_trot(t)
 
 # desired COP is mean of leg points
-p = np.zeros((2, N))
+p_ref = np.zeros((2, N))
 for n in range(N):
-    p[0, n] = np.sum(x[leg_state[n], n])/np.sum(leg_state[n])
-    p[1, n] = np.sum(y[leg_state[n], n])/np.sum(leg_state[n])
+    p_ref[0, n] = np.sum(x[leg_state[n], n])/np.sum(leg_state[n])
+    p_ref[1, n] = np.sum(y[leg_state[n], n])/np.sum(leg_state[n])
     
-<<<<<<< HEAD
-#p_zmp = zmp.get_zmp(p)
-=======
-p_zmp = zmp.get_zmp(p)
->>>>>>> quaroDance/main
+# calculate ZMP spline
+spline_zmp = zmp.optimize_ZMP_spline(p_ref, T_hor, 
+                                     alpha=1e4, beta=1e-4, gamma=1e-6)
+p_zmp = spline_zmp(t)
 
 ### Plot ####################################################################
-fig, (axx, axz) = plt.subplots(2, 1)
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
 fig.set_figheight(10)
-fig.set_figwidth(10)
 
 for n in range(4):
-    axx.plot(x[n], y[n], label=('x'+str(n)))
-<<<<<<< HEAD
-    axz.plot(t, z[n], label=('xz'+str(n)))
-    for m in range(N):
-        if leg_state[m, n]:
-            c="k"
-        else:
-            c="w"
-        axz.plot(t[m], n/4, "o", color=c)
+    ax1.plot(x[n], y[n], label=('x'+str(n)))
+    #ax2.plot(t, x[n])
+    #ax3.plot(t, y[n])
+
+ax1.plot(p_ref[0], p_ref[1], "--", label="COMref")
+ax1.plot(p_zmp[0], p_zmp[1], label="ZMP")   
+
+ax2.plot(t, p_ref[0], "--", label="x_COMref")
+ax2.plot(t, p_zmp[0], label="x_ZMP")
+#ax2.plot(t, p_zmp[0]-p_ref[0], "--", label="del")
 
 
-#axz.plot(t, p_zmp[0], label="ZMP")
-axx.plot(p[0], p[1], label="COM")
-#axx.plot(p_zmp[0], p_zmp[1], label="ZMP")   
-
-=======
-    axz.plot(t, x[n])
-    axz.plot(t, p_zmp[0])
-
-axx.plot(p_zmp[0], p_zmp[1], label="ZMP")   
-axx.plot(p[0], p[1], label="COM")
->>>>>>> quaroDance/main
-
-# plot a single locus
-#M = 1
-#for n in range(4):
-#    axz.plot(x[n, M], y[n, M], "o", label=('x'+str(n)))
-#axz.plot(p[0, M], p[1, M], "o", label="COM")
+ax3.plot(t, p_ref[1], "--", label="y_COMref")
+ax3.plot(t, p_zmp[1], label="y_ZMP")
+#ax3.plot(t, p_zmp[1]-p_ref[1], "--", label="del")
     
-axx.legend()
-axx.axis('equal')
-axz.legend()
-axz.axis('equal')
+ax1.legend()
+ax1.axis('equal')
+ax2.legend()
+ax2.set_ylim(-0.4,0.4)
+ax3.legend()
+ax3.set_ylim(-0.4,0.4)
     
--- src/zero_moment_point.py
+++ src/zero_moment_point.py
@@ -6,7 +6,7 @@ Sources:
     
 https://www.youtube.com/watch?v=GnFaLl7qwco
 http://myweb.sabanciuniv.edu/selimozel/files/2012/10/Zero-Moment-Point-Based-Pace-Reference-Generation-for-Quadruped-Robots-via-Preview-Control.pdf
-<<<<<<< HEAD
+
 
 The optimization is done using OSQP which requires the following problem
 formulation:
@@ -14,13 +14,10 @@ formulation:
 minimize        0.5 x' P x + q' x
 subject to      l <= A x <= u
 
-=======
->>>>>>> quaroDance/main
 """
 
 
 import numpy as np
-<<<<<<< HEAD
 import osqp
 import time
 from scipy.interpolate import CubicHermiteSpline as hspline
@@ -28,20 +25,26 @@ from scipy import sparse
 
 # constants
 g = 9.81            # gravitation
-z_c = 0.25#0.615    # COM hight
-alpha = 1e3#10      # weighting factor
-beta = 1e-3         # weighting factor
-gamma = 1e-6#0      # weighting factor
 
-
-def calculate_ZMP_spline(p_ref, T_hor):
+def optimize_ZMP_spline(
+        p_ref,
+        T_hor,
+        alpha=1e4, beta=1e-4, gamma=1e-6,
+        z_c = 0.25
+        ):
     '''
-    calculate optimal ZMP and and resulting spline
+    calculate optimal ZMP and and resulting spline. Algorithm assumes equally
+    distributed reference points over time T_hor! 
 
     Parameters
     ----------
-    p_ref : (2xN) np.ndarray containing reference points in x/y dimension
-    T_hor : float optimization horizon length
+    p_ref : (2xN) np.ndarray, containing reference points in x/y dimension
+    T_hor : float, optimization horizon length
+    T_hor : float, optimization horizon length
+    alpha : float, weighting factor for (p_com - p_ref)
+    beta :  float, weighting factor for control values
+    gamma : float, weighting factor for state values
+    z_c : float, COM hight
 
     Returns
     -------
@@ -184,7 +187,7 @@ if __name__=="__main__":
     
     
     # calculate spline containing optimal ZMP
-    spline = calculate_ZMP_spline(p_ref, T_tot)
+    spline = optimize_ZMP_spline(p_ref, T_tot)
     
     
     # plot optimal ZMP
@@ -199,97 +202,5 @@ if __name__=="__main__":
     p_ref = sample_ref_traj(t_plt)
     plt.plot(t_plt, p_ref[0], '--', label="reference")
     plt.legend()
-=======
-import quadprog
-
-# constants
-m = 1.
-g = 9.81
-z_c = 40.    # COM hight
-N = 20       # horizon steps
-T_tot = 1.5 # horizon length
-
-T = T_tot/N
-n_u = N    # total number of controls to optimize per dimension
-n_x = 3*N  # total number of states to optimize per dimension
-n_p = 1    # total number of output variables per dimension
-
-
-# original state space matrices
-A = np.array([[1, T, (T**2)/2],
-              [0, 1,        T],
-              [0, 0,        1]])
-
-B = np.array([[(T**3)/6],
-              [(T**2)/2],
-              [T]])
-
-C = np.array([1, 0, -z_c/g])
-
-# state space matrices extended to number of steps
-A_e = np.kron(np.eye(N), A)
-B_e = np.kron(np.eye(N), B)
-C_e = np.kron(np.eye(N), C)
-
-# state space matrices extended to fit 'extended' optvar containing states AND control input
-O_x  = np.zeros((n_x, n_x))
-O_xu = np.zeros((n_x, n_u))
-O_ux = np.zeros((n_u, n_x))
-O_u  = np.zeros((n_u, n_u))
-O_p = np.zeros((N*n_p, n_u))
-O_c = np.zeros(C_e.shape)
-
-A_q = np.block([[A_e,  O_x,  O_xu, O_xu],
-                [O_x,  A_e,  O_xu, O_xu],
-                [O_ux, O_ux, O_u,  O_u],
-                [O_ux, O_ux, O_u,  O_u]])
-
-B_q = np.block([[O_x,  O_x,  B_e,  O_xu],
-                [O_x,  O_x,  O_xu, B_e],
-                [O_ux, O_ux, O_u,  O_u],
-                [O_ux, O_ux, O_u,  O_u]])
-
-C_q = np.block([[C_e, O_c, O_p, O_p],
-                [O_c, C_e, O_p, O_p]])
-
-
-# weighting factors
-S_q = np.eye(2*N)
-#S_q[2*n_x:] = 0              # penalize only state values
-R = np.eye(2*(n_x + n_u))
-#R[:2*n_x] = 0                # penalize only control values
-
-
-def get_zmp(p):
-    # for tests:
-    p_ref = np.copy(p.flatten())
-    
-    # convert matrices into a QP
-    G_tild = 2*(C_q.T@S_q@C_q + R)
-    a_tild = 2*p_ref.T@S_q@C_q
-    I = np.ones(2*(n_x + n_u))
-    O = np.zeros((2*(n_x + n_u), 1))
-    C_tild = np.vstack( (I-A_q-B_q,
-                       -(I-A_q-B_q))).T
-    b_tild = np.vstack((O, O))[:, 0]        # index necessary for right shape
-    
-    # solve QP
-    sol = quadprog.solve_qp(G_tild, a_tild, C_tild, b_tild)[0]
-    result = C_q@sol
-    
-    p_zmp = np.zeros(p.shape)
-    p_zmp[0] = result[:N]
-    p_zmp[1] = result[N:]
-    return p_zmp
-
-
-
-# for tests
-if __name__=="__main__":
-    p = np.array([[ 0.625     , -5.        ,  6.25      ,  0.625     , -5.        ],
-       [ 0.        ,  3.33333333, -3.33333333,  0.        ,  3.33333333]])
-    #print(get_zmp(np.random.rand(2, N)))
-    print(get_zmp(p/10))
->>>>>>> quaroDance/main
 
 
